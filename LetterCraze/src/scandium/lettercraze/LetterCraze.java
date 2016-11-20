a96337d5a1de700bd46f43e89002ee87b5cf6bf8
Kodey Converse <krconverse@wpi.edu>
2016-11-20 12:26:26 -0500

    Added the main LetterCraze class

---
+++ LetterCraze/src/scandium/lettercraze/LetterCraze.java
@@ -0,0 +1,28 @@
+/**
+ * LetterCraze.java
+ * 
+ * @author Kodey Converse (kodey@krconv.com)
+ */
+package scandium.lettercraze;
+
+/**
+ * 
+ */
+public class LetterCraze {
+
+	/**
+	 * 
+	 */
+	public LetterCraze() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
