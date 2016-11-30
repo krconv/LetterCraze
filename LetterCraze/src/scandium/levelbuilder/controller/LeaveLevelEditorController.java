61af0388f04a359b12d6b2bb40d59c8a3dd4219e
Kodey Converse <krconverse@wpi.edu>
2016-11-29 21:51:53 -0500

    Add Felix's LeaveLevelEditor controller

---
+++ LetterCraze/src/scandium/levelbuilder/controller/LeaveLevelEditorController.java
@@ -0,0 +1,32 @@
+package scandium.levelbuilder.controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import scandium.levelbuilder.model.Model;
+import scandium.levelbuilder.view.Application;
+
+public class LeaveLevelEditorController extends MouseAdapter{
+
+	//attributes
+    Model model;
+    Application app;
+
+    
+    /**
+     * @param model 
+     * @param app
+     */
+    public LeaveLevelEditorController(Model m, Application a) {
+        this.model = m;
+        this.app = a;
+    }
+
+    
+    /**
+     * @param MouseEvent me
+     */
+    public void mouseClicked(MouseEvent me) {
+    	app.setViewMainMenu();
+    }
+}
