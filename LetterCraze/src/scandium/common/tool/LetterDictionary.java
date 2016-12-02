3c951ed21b3d9eabd80a1365d0c63a68b0daa626
Kodey Converse <krconverse@wpi.edu>
2016-12-02 18:33:05 -0500

    Adds a seed field to LetterDictionary
    This not only helps with testing, but could be useful in the production
    code as well (if we wanted to guarentee that a level could be won).

--- LetterCraze/src/scandium/common/tool/LetterDictionary.java
+++ LetterCraze/src/scandium/common/tool/LetterDictionary.java
@@ -28,10 +28,21 @@ public class LetterDictionary {
      * @param scores The list of scores corresponding to the letters.
      */
     public LetterDictionary(String[] letters, double[] frequencies, int[] scores) {
+    	this(letters, frequencies, scores, System.currentTimeMillis());
+    }
+    
+    /**
+     * Creates a new LetterDictionary.
+     * @param letters The letters which will be used to generate Tiles. 
+     * @param frequencies A list of frequencies corresponding to the letters.
+     * @param scores The list of scores corresponding to the letters.
+     * @param seed The seed for the random generator.
+     */
+    public LetterDictionary(String[] letters, double[] frequencies, int[] scores, long seed) {
     	this.letters = letters;
     	this.frequencies = frequencies;
     	this.scores = scores;
-    	this.random = new Random();
+    	this.random = new Random(seed);
     }
 
     /**
